f02a0bc9932f822d231666379a12e2b2fdeec607
EngredV <vanegas.engred@gmail.com>
2021-01-11 13:29:22 -0500

    Fixed all files

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import ContactCard from "./ContactCard";
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+      return (
+          <>
+          <ContactCard
+              name="Ashley Rivers"
+              mobile="5168987415"
+              work="2127086414"
+              email="AshleyR@gmail.com"
+              />
+          <ContactCard
+              name="John Smith"
+              mobile="9294589413"
+              work="7182050555"
+              email="SmithJ@gmail.com"
+              /> 
+          <ContactCard
+              name="Robert Jones"
+              mobile="9173204222"
+              work="2127086414"
+              email="Robert.Jones@gmail.com"
+              />
+  </>        
+);
+  }
+}
+
+export default App;
